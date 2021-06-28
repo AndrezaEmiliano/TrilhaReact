143cca94feff9a0a9abf84cf641b41e81468d9e5
AndrezaEmiliano <andreza.emiliano@exactaworks.com.br>
2021-06-28 14:30:58 -0300

    create pagination and api

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,40 +1,68 @@
 import { useEffect, useCallback, useState } from "react";
-import axios from "axios";
 
 import TaskForm from "../../components/TaskForm";
 import TaskList from "../../components/TaskList";
+import Pagination from "../../components/Pagination";
 import * as S from "./styles";
 import { Tasks } from "../../services/tasks";
 
-function Home() {
-  const [tasks, setTasks] = useState([]);
+const limit = 5;
 
-  const refreshTasks = useCallback(async () => {
-    const response = await Tasks.getTasks();
-    setTasks(response);
+function Home() {
+  const [totalPages, setTotalPages] = useState([]);
+  const [tasks, setTasks] = useState([]);
+  const [page, setPage] = useState(1);
+
+  const refreshTasks = useCallback(async (page) => {
+    const { data, total } = await Tasks.getTasks(page, limit);
+
+    setTasks(data);
+    setTotalPages(Math.ceil(total / limit));
   }, []);
 
   async function createTask(text) {
     await Tasks.createTask({ text });
 
-    refreshTasks();
+    refreshTasks(page);
   }
 
   async function removeTask(task) {
     await Tasks.deleteTask(task.id);
 
-    refreshTasks();
+    refreshTasks(page);
+  }
+
+  function handlePrevious() {
+    if (page > 1) {
+      setPage(page - 1);
+    } else {
+      return alert("Você já esta na primeira página");
+    }
+  }
+
+  function handleNext() {
+    if (page < totalPages) {
+      setPage(page + 1);
+    }
   }
 
   useEffect(() => {
-    refreshTasks();
-  }, [refreshTasks]);
+    refreshTasks(page);
+  }, [refreshTasks, page]);
 
   return (
     <S.Wrapper>
       <TaskForm onSubmit={createTask} />
 
       <TaskList tasks={tasks} removeTask={removeTask} />
+
+      <Pagination
+        firstPage={page === 1}
+        lastPage={page === totalPages}
+        page={page}
+        onNext={handleNext}
+        onPrevious={handlePrevious}
+      />
     </S.Wrapper>
   );
 }
